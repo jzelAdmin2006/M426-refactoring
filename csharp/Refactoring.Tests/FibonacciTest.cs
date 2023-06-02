742da394f53416f5203b6ff2f9d3127130f40ee9
Johannes Zeller <johannes_zeller@sluz.ch>
2023-06-02 13:51:03 +0200

    improve performance

--- csharp/Refactoring.Tests/FibonacciTest.cs
+++ csharp/Refactoring.Tests/FibonacciTest.cs
@@ -3,21 +3,17 @@ using Xunit;
 
 namespace Refactoring.Tests;
 
-public class FibonacciTest
-{
-    [Fact]
-    public void TestFirstFibonacciNumberIsOne()
+public class FibonacciTest {
+    [Theory]
+    [InlineData(0, 1)]
+    [InlineData(1, 1)]
+    [InlineData(2, 2)]
+    [InlineData(3, 3)]
+    [InlineData(4, 5)]
+    [InlineData(5, 8)]
+    [InlineData(45, 1836311903)]
+    public void TestFibonacciNumbers(int n, int expected)
     {
-        // TODO: write test
-    }
-
-    [Fact]
-    public void TestBigFibonacciNumber()
-    {
-        // Arrange
-        int n = 45;
-        int expected = 1836311903;
-
         // Act
         int actual = Fibonacci.Fib(n);
 
--- csharp/Refactoring/Fibonacci.cs
+++ csharp/Refactoring/Fibonacci.cs
@@ -1,17 +1,19 @@
-namespace Refactoring;
+using System;
+using System.Linq;
 
-public class Fibonacci
-{
-    public static int Fib(int n)
-    {
-        if (n < 0)
+namespace Refactoring {
+    public static class Fibonacci {
+        public static int Fib(int n)
         {
-            throw new ArgumentException("Fibonacci undefined for negative numbers");
+            if (n < 0)
+            {
+                throw new ArgumentException("Fibonacci undefined for negative numbers");
+            }
+            return n == 0 || n == 1 ? 1 :
+                Enumerable.Range(2, n - 1)
+                .Aggregate((prev: 1, next: 1),
+                    (fibN, _) => (fibN.next, fibN.prev + fibN.next))
+                .next;
         }
-        if (n == 0 || n == 1)
-        {
-            return 1;
-        }
-        return Fib(n - 2) + Fib(n - 1);
     }
 }
