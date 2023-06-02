5b442a20cc372f7744d5bda0e7ceae4d1122411c
Johannes Zeller <johannes_zeller@sluz.ch>
2023-06-02 14:11:41 +0200

    improve flexibility

--- csharp/Refactoring/CurrencyConverter.cs
+++ csharp/Refactoring/CurrencyConverter.cs
@@ -2,20 +2,23 @@ namespace Refactoring;
 
 public class CurrencyConverter
 {
+    private static Dictionary<string, double> exchangeRates = new()
+        {
+            { "USD", 1.09 },
+            { "EUR", 0.96 },
+            { "GBP", 0.82 },
+            { "RUB", 79.78 }
+        };
+
     public static double ConvertTo(double amount, string toCurrency)
     {
-        switch (toCurrency)
+        if(exchangeRates.ContainsKey(toCurrency))
         {
-            case "USD":
-                return amount * 1.09;
-            case "EUR":
-                return amount * 0.96;
-            case "GBP":
-                return amount * 0.82;
-            case "RUB":
-                return amount * 79.78;
-            default:
-                throw new ArgumentException($"no exchange rate for {toCurrency} available");
+            return amount * exchangeRates[toCurrency];
+        }
+        else
+        {
+            throw new ArgumentException($"no exchange rate for {toCurrency} available");
         }
     }
 }
