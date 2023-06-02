6cde18ea47859e41aeea560d915917c669b1331d
Johannes Zeller <johannes_zeller@sluz.ch>
2023-06-02 13:19:21 +0200

    improve scalability

--- csharp/Refactoring.Tests/FamilyMemberTest.cs
+++ csharp/Refactoring.Tests/FamilyMemberTest.cs
@@ -39,4 +39,28 @@ public class FamilyMemberTest
         Assert.Equal(2, descendants[b]);
         Assert.Equal(1, descendants[c]);
     }
+
+    [Fact]
+    public void TestDescendantsOfNGenerationsHaveCorrectGeneration()
+    {
+        // Arrange
+        FamilyMember a = new("a");
+        FamilyMember b = new("b", new() { a });
+        FamilyMember c = new("c", new() { b });
+        FamilyMember d = new("d", new() { c });
+        FamilyMember e = new("e", new() { d });
+        FamilyMember f = new("f", new() { e });
+        FamilyMember g = new("g", new() { f });
+
+        // Act
+        var descendants = e.GetDescendants(0);
+
+        // Assert
+        Assert.Equal(6, descendants[a]);
+        Assert.Equal(5, descendants[b]);
+        Assert.Equal(4, descendants[c]);
+        Assert.Equal(3, descendants[d]);
+        Assert.Equal(2, descendants[e]);
+        Assert.Equal(1, descendants[f]);
+    }
 }
