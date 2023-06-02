cd3749f1ae6e484f0e9f29dc854c7471d1c12d01
Johannes Zeller <johannes_zeller@sluz.ch>
2023-06-02 14:28:14 +0200

    improve flexibility

--- csharp/Refactoring.Tests/RoundingTest.cs
+++ csharp/Refactoring.Tests/RoundingTest.cs
@@ -2,5 +2,36 @@ namespace Refactoring.Tests;
 
 public class RoundingTest
 {
-    // TODO
+    [Theory]
+    [InlineData(1.0, 1.0, 1.0)]
+    [InlineData(1.0, 1.1, 1.0)]
+    [InlineData(1.0, 1.5, 2.0)]
+    [InlineData(1.0, 1.9, 2.0)]
+    [InlineData(0.5, 1.0, 1.0)]
+    [InlineData(0.5, 1.1, 1.0)]
+    [InlineData(0.5, 1.5, 1.5)]
+    [InlineData(0.5, 1.9, 2.0)]
+    [InlineData(0.1, 1.0, 1.0)]
+    [InlineData(0.1, 1.1, 1.1)]
+    [InlineData(0.1, 1.5, 1.5)]
+    [InlineData(0.1, 1.9, 1.9)]
+    [InlineData(0.05, 1.0, 1.0)]
+    [InlineData(0.05, 1.1, 1.1)]
+    [InlineData(0.05, 1.5, 1.5)]
+    [InlineData(0.05, 1.9, 1.9)]
+    [InlineData(0.01, 1.0, 1.0)]
+    [InlineData(0.01, 1.1, 1.1)]
+    [InlineData(0.01, 1.5, 1.5)]
+    [InlineData(0.01, 1.9, 1.9)]
+    public void Round(double precision, double value, double expected)
+    {
+        // Arrange
+        var rounding = new Rounding(precision);
+
+        // Act
+        var actual = rounding.Round(value);
+
+        // Assert
+        Assert.Equal(expected, actual, 15);
+    }
 }
--- csharp/Refactoring/Rounding.cs
+++ csharp/Refactoring/Rounding.cs
@@ -2,7 +2,7 @@ namespace Refactoring;
 
 public class Rounding
 {
-    public double Precision { get; }
+    private readonly double Precision;
 
     public Rounding(double precision)
     {
@@ -11,20 +11,6 @@ public class Rounding
 
     public double Round(double value)
     {
-        switch (Precision)
-        {
-            case 1.0:
-                return Math.Round(value);
-            case 0.5:
-                return Math.Round(value * 2.0) / 2.0;
-            case 0.1:
-                return Math.Round(value * 10.0) / 10.0;
-            case 0.05:
-                return Math.Round(value * 20.0) / 20.0;
-            case 0.01:
-                return Math.Round(value * 100.0) / 100.0;
-            default:
-                throw new ArgumentException($"unable to round to a precision of {Precision}");
-        }
+        return Math.Round(value / Precision) * Precision;
     }
 }
